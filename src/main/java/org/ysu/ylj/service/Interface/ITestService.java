98ba24578e2ff2e8ce7aecc71797d249a31b1610
gityanglijun <yanglijun3.14@qq.com>
2019-02-11 16:49:03 +0800

    第一次创建

--- src/main/java/org/ysu/ylj/service/Interface/ITestService.java
+++ src/main/java/org/ysu/ylj/service/Interface/ITestService.java
@@ -1,5 +1,7 @@
 package org.ysu.ylj.service.Interface;
 
+import org.ysu.ylj.pojo.Table1;
+
 import java.util.List;
 
 public interface ITestService {
--- src/main/java/org/ysu/ylj/service/achievements/TestServiceImpl.java
+++ src/main/java/org/ysu/ylj/service/achievements/TestServiceImpl.java
@@ -1,6 +1,9 @@
 package org.ysu.ylj.service.achievements;
 
 import org.springframework.stereotype.Service;
+import org.ysu.ylj.dao.Table1Mapper;
+import org.ysu.ylj.pojo.Table1;
+import org.ysu.ylj.pojo.Table1Example;
 import org.ysu.ylj.service.Interface.ITestService;
 
 import javax.annotation.Resource;
@@ -8,13 +11,13 @@ import java.util.List;
 
 @Service
 public class TestServiceImpl implements ITestService {
-    //@Resource
-    //Table1Mapper table1Mapper;
+    @Resource
+    Table1Mapper table1Mapper;
 
-   /* @Override
+   @Override
     public List<Table1> getTables() {
         Table1Example table1Example=new Table1Example();
         List<Table1> lists=table1Mapper.selectByExample(table1Example);
         return lists;
-    }*/
+    }
 }
